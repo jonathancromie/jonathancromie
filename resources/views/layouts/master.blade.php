b02a4a322a6216e61c23147ded2e3064ef6c7a34
Jonathan Cromie <joncromie@gmail.com>
2016-01-03 20:21:12 +1000

    Added template and master, index blades

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<!--
+	Interphase by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>@yield('title')</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+		<header id="header">
+			<h1><a href="index">Jonathan Cromie</a></h1>
+			<nav id="nav">
+				<ul>
+					<li><a href="{{ asset('index') }}">Home</a></li>
+					<li><a href="{{ asset('portfolio') }}">Portfolio</a></li>
+					<li><a href="{{ asset('blog') }}">Blog</a></li>
+					<li><a href="{{ asset('resume') }}">Resume</a></li>
+					<li><a href="{{ asset('contact') }}">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+
+		@yield('content')
+
+		<!-- Footer -->
+		<footer id="footer">
+			<div class="container">
+				<div class="row">
+					<section class="4u 6u(medium) 12u$(small)">
+						<h3>Lorem ipsum</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, cumque!</p>
+						<ul class="alt">
+							<li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+							<li><a href="#">Quod adipisci perferendis et itaque.</a></li>
+							<li><a href="#">Itaque eveniet ullam, veritatis reiciendis?</a></li>
+							<li><a href="#">Accusantium repellat accusamus a, soluta.</a></li>
+						</ul>
+					</section>
+					<section class="4u 6u$(medium) 12u$(small)">
+						<h3>Nostrum, repellat!</h3>
+						<p>Tenetur voluptate exercitationem eius tempora! Obcaecati suscipit, soluta earum blanditiis.</p>
+						<ul class="alt">
+							<li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+							<li><a href="#">Id inventore, qui necessitatibus sunt.</a></li>
+							<li><a href="#">Deleniti eum odit nostrum eveniet.</a></li>
+							<li><a href="#">Illum consectetur quibusdam eos corporis.</a></li>
+						</ul>
+					</section>
+					<section class="4u$ 12u$(medium) 12u$(small)">
+						<h3>Contact Us</h3>
+						<ul class="icons">
+							<li><a href="#" class="icon rounded fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon rounded fa-facebook"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon rounded fa-pinterest"><span class="label">Pinterest</span></a></li>
+							<li><a href="#" class="icon rounded fa-google-plus"><span class="label">Google+</span></a></li>
+							<li><a href="#" class="icon rounded fa-linkedin"><span class="label">LinkedIn</span></a></li>
+						</ul>
+						<ul class="tabular">
+							<li>
+								<h3>Address</h3>
+								1234 Somewhere Road<br>
+								Nashville, TN 00000
+							</li>
+							<li>
+								<h3>Mail</h3>
+								<a href="#">someone@untitled.tld</a>
+							</li>
+							<li>
+								<h3>Phone</h3>
+								(000) 000-0000
+							</li>
+						</ul>
+					</section>
+				</div>
+				<ul class="copyright">
+					<li>&copy; Untitled. All rights reserved.</li>
+					<li>Design: <a href="http://templated.co">TEMPLATED</a></li>
+					<li>Images: <a href="http://unsplash.com">Unsplash</a></li>
+				</ul>
+			</div>
+		</footer>
+	</body>
+</html>
