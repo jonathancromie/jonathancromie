895549ed7a62fe41de421d3dcf16d3162aabccc5
Jonathan Cromie <joncromie@gmail.com>
2016-01-04 21:37:09 +1000

    Increased padding to bottom of banner

--- public/js/scrollTo.js
+++ public/js/scrollTo.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 	$('a[href="#one"]').click(function() {
 	    var offset = 20; //Offset of 20px
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -82,5 +82,5 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="{{ asset('js/scrollTo.js') }}"></script>
-	
+
 @endsection
